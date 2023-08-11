17a59f3bf6630fc6db76f4a79f8283da761275e9
Kiza45 <kieransharp2@gmail.com>
2023-08-11 14:21:39 +0100

    script will now only run if its a week day

--- pyradio.py
+++ pyradio.py
@@ -2,8 +2,8 @@ import vlc
 import time
 import schedule
 from itertools import cycle
-from datetime import datetime
-now = datetime.now()
+import datetime as d
+
 
 # below is a list of URL/m3u/plsd files to play
 urls = [
@@ -12,6 +12,7 @@ urls = [
     'file:///C:/PyRadio/radio1.pls',
     'file:///C:/PyRadio/virgin.m3u',
     'file:///C:/PyRadio/jassfm.pls',
+    'file:///C:/PyRadio/kerrang.pls',
     'file:///C:/PyRadio/captialfm.m3u',
     'file:///C:/PyRadio/capitalxtra.m3u',
     # 'file:///C:/PyRadio/magic.pls',
@@ -19,47 +20,43 @@ urls = [
 
 ]
     
-
+days = ["monday", "tuesday", "wednesday", "thursday", "friday"]
 # playlists = set(['pls','m3u'])
 
 
-def radioPlayScheduler():
+def radio_player():
+    today = d.date.today()
+    if today.weekday() < 5 & today.weekday() >= 0:
+        # setting how long to run the radio for
+        day_start = time.time()
+        day_end = day_start + 8.5*3600
+        while time.time() <= day_end:
+            while True:
+                instance = vlc.Instance()
 
-    # setting how long to run the radio for
-    day_start = time.time()
-    day_end = day_start + 8.5*3600
-    while time.time() <= day_end:
-        while True:
-            instance = vlc.Instance()
+                # cycle through the list of urls
+                # repeatedly until 17:30
+                for url in cycle(urls):
+                    start = time.time()
+                    change = start + 30 * 60
+                    while start <= change:
+                        player = instance.media_player_new()
+                        media = instance.media_new(url)
+                        media_list = instance.media_list_new([url])
+                        media.get_mrl()
+                        player.set_media(media)
 
-            # cycle through the list of urls
-            # repeatedly until 17:30
-            for url in cycle(urls):
-                start = time.time()
-                change = start + 30 * 60
-                while start <= change:
-                    player = instance.media_player_new()
-                    media = instance.media_new(url)
-                    media_list = instance.media_list_new([url])
-                    media.get_mrl()
-                    player.set_media(media)
+                        list_player = instance.media_list_player_new()
+                        list_player.set_media_list(media_list)
+                        player.audio_set_volume(100)
+                        if list_player.play() == -1:
+                            print("Error playing")
+                        else:
+                            s2 = "Playing Radio"
+                            print(s2)
 
-                    list_player = instance.media_list_player_new()
-                    list_player.set_media_list(media_list)
-                    player.audio_set_volume(100)
-                    if list_player.play() == -1:
-                        print("Error playing")
-                    else:
-                        s2 = "Playing Radio"
-                        print(s2)
 
-    
-schedule.every().monday.at("09:00").do(radioPlayScheduler())
-schedule.every().tuesday.at("09:00").do(radioPlayScheduler())
-schedule.every().wednesday.at("09:00").do(radioPlayScheduler())
-schedule.every().thursday.at("09:00").do(radioPlayScheduler())
-schedule.every().friday.at("09:00").do(radioPlayScheduler())
-
+schedule.every().at("09:00").do(radio_player())
 while True:
     # Checks whether a scheduled task
     # is pending to run or not
