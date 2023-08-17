2bf400f29f564684f828838be10017b17e204afc
Kiza45 <kieransharp2@gmail.com>
2023-08-17 16:59:10 +0100

    tried to change the timing method and the skip method. Not working properly yet

--- pyradio.py
+++ pyradio.py
@@ -28,60 +28,69 @@ def radio_player():
     vlc_path = r"C:\Program Files\VideoLAN\VLC\vlc.exe"
     print(d.datetime.now().hour)
     print(d.datetime.now().minute)
+    run_time = 10
 
-    # setting how long to run the radio for
-    day_start = time.time()
-    day_end = day_start + 8.5*3600
-    while time.time() <= day_end:
-        while True:
-            playlist_url = urls[current_index]
-            with open(playlist_url, "r") as radio_file:
-                radio_content = radio_file.read()
+    while True:
+        playlist_url = urls[current_index]
+        with open(playlist_url, "r") as radio_file:
+            radio_content = radio_file.read()
 
-            if "m3u" in playlist_url:
+        if "m3u" in playlist_url:
 
-                radio_lines = radio_content.splitlines()
-                radio_entries = [line for line in radio_lines if line and not line.startswith("#")]
+            radio_lines = radio_content.splitlines()
+            radio_entries = [line for line in radio_lines if line and not line.startswith("#")]
 
-                # stream_url = None
-                # for line in radio_content.splitlines():
-                #    if line.startswith("File1="):
-                #      stream_url = line[len("File1="):]
-                #      print(stream_url)
-                #     break
+            # stream_url = None
+            # for line in radio_content.splitlines():
+            #    if line.startswith("File1="):
+            #      stream_url = line[len("File1="):]
+            #      print(stream_url)
+            #     break
 
-                if radio_entries:
-
-                    stream_url = radio_entries[0]
-                    print(stream_url)
+            if radio_entries:
+                start_time = time.time()
+                stream_url = radio_entries[0]
+                print(stream_url)
+                while time.time() - start_time < run_time:
+                    user_input = input("Type 's' to skip stations: ")
                     vlc_process = subprocess.Popen([vlc_path, "--playlist-autostart", stream_url])
-
-                    time.sleep(15)
-
-                    vlc_process.terminate()  # Terminate the current VLC instance
-
-                    if current_index == (len(urls)-1):
-                        current_index = 0
-                    else:
-                        current_index = (current_index + 1)
-
-            if "pls" in playlist_url:
-                stream_url = None
-                for line in radio_content.splitlines():
-                    if line.startswith("File1="):
-                        stream_url = line[len("File1="):]
-                        print(stream_url)
+                    # time.sleep(15)   Sleep for 30 minutes (1800 seconds)
+                    if user_input.lower() == 'skip':
+                        vlc_process.terminate()
+                        print("Skipping stations.")
                         break
 
-                if stream_url:
-                    vlc_process = subprocess.Popen([vlc_path, "--playlist-autostart", stream_url])
-                    time.sleep(15)  # Sleep for 30 minutes (1800 seconds)
-                    vlc_process.terminate()  # Terminate the current VLC instance
+                vlc_process.terminate()  # Terminate the current VLC instance
 
-                    if current_index == (len(urls) - 1):
-                        current_index = 0
-                    else:
-                        current_index = (current_index + 1)
+                if current_index == (len(urls)-1):
+                    current_index = 0
+                else:
+                    current_index = (current_index + 1)
+
+        if "pls" in playlist_url:
+            stream_url = None
+            for line in radio_content.splitlines():
+                if line.startswith("File1="):
+                    stream_url = line[len("File1="):]
+                    print(stream_url)
+                    break
+
+            if stream_url:
+                start_time = time.time()
+                while time.time() - start_time < run_time:
+                    user_input = input("Type 's' to skip stations: ")
+                    vlc_process = subprocess.Popen([vlc_path, "--playlist-autostart", stream_url])
+                    # time.sleep(15)   Sleep for 30 minutes (1800 seconds)
+                    if user_input.lower() == 'skip':
+                        vlc_process.terminate()
+                        print("Skipping stations.")
+                        break
+                vlc_process.terminate()  # Terminate the current VLC instance
+
+                if current_index == (len(urls) - 1):
+                    current_index = 0
+                else:
+                    current_index = (current_index + 1)
 
 
 radio_player()
